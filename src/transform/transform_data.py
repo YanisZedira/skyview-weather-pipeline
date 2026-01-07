7f5dfd88b8c4f4b07501f7b752cfa8ef05b84c40
Yanis Zedira <155983383+YanisZedira@users.noreply.github.com>
2026-01-07 10:52:24 +0100

    Enhance transform_weather with logging and error handling
    Added error handling and logging to the transform_weather function.

--- src/transform/transform_data.py
+++ src/transform/transform_data.py
@@ -1,14 +1,27 @@
-from datetime import timezone
+from utils.logger import get_logger
+
+logger = get_logger("TRANSFORM")
 
 def transform_weather(weather, air, city, now):
-    return {
-        "city": city["city"],
-        "continent": city["continent"],
-        "date": str(now.date()),
-        "hour": now.hour,
-        "temperature_c": round(weather["main"]["temp"] - 273.15, 2),
-        "humidity": weather["main"]["humidity"],
-        "wind_speed": weather["wind"]["speed"],
-        "air_quality_index": air["list"][0]["main"]["aqi"],
-        "ingestion_timestamp": now.isoformat()
-    }
+    try:
+        if weather is None or air is None:
+            raise ValueError("Missing input data")
+
+        row = {
+            "city": city["city"],
+            "continent": city["continent"],
+            "date": str(now.date()),
+            "hour": now.hour,
+            "temperature_c": round(weather["main"]["temp"] - 273.15, 2),
+            "humidity": weather["main"]["humidity"],
+            "wind_speed": weather["wind"]["speed"],
+            "air_quality_index": air["list"][0]["main"]["aqi"],
+            "ingestion_timestamp": now.isoformat()
+        }
+
+        logger.info(f"Data transformed for {city['city']}")
+        return row
+
+    except Exception as e:
+        logger.error(f"Transform failed for {city['city']} - {e}")
+        return None
