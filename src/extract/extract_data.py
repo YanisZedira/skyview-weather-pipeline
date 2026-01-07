a2153854ddcb748a7a5cbe5a2fc62c0845d82c38
Yanis Zedira <155983383+YanisZedira@users.noreply.github.com>
2026-01-07 10:51:59 +0100

    Update extract_data.py

--- src/extract/extract_data.py
+++ src/extract/extract_data.py
@@ -1,12 +1,25 @@
+from utils.logger import get_logger
 import requests
 
+logger = get_logger("EXTRACT")
+
 def extract_weather(city, api_key):
-    weather = requests.get(
-        f"https://api.openweathermap.org/data/2.5/weather?lat={city['lat']}&lon={city['lon']}&appid={api_key}"
-    ).json()
+    try:
+        weather_url = (
+            f"https://api.openweathermap.org/data/2.5/weather"
+            f"?lat={city['lat']}&lon={city['lon']}&appid={api_key}"
+        )
+        air_url = (
+            f"https://api.openweathermap.org/data/2.5/air_pollution"
+            f"?lat={city['lat']}&lon={city['lon']}&appid={api_key}"
+        )
 
-    air = requests.get(
-        f"https://api.openweathermap.org/data/2.5/air_pollution?lat={city['lat']}&lon={city['lon']}&appid={api_key}"
-    ).json()
+        weather = requests.get(weather_url, timeout=10).json()
+        air = requests.get(air_url, timeout=10).json()
 
-    return weather, air
+        logger.info(f"Weather data extracted for {city['city']}")
+        return weather, air
+
+    except Exception as e:
+        logger.error(f"Extract failed for {city['city']} - {e}")
+        return None, None
